19809459bd7691bbdfb4996b9d9fe114ee1d0728
noor1449 <imannoorul44@gmail.com>
2026-06-24 17:59:25 +0500

    Add counter project

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import Newfile from './Newfile'
+
+function App() {
+  return (
+    <Newfile/>
+  )
+}
+
+export default App
---
+++ vitereact/src/Newfile.jsx
@@ -0,0 +1,7 @@
+function Newfile(){
+    return(
+        <h1>hello world</h1>
+    )
+}
+
+export default Newfile
